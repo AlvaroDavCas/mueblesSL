605e9cff50366fb98b0dff59a4dbd7be182ec0ff
DCA <paquirri@no-molestar.es>
2026-01-08 19:05:14 +0100

    Añadir la entidad factura

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
                 "C/CALLE", "LUGO", "", "CORREO@correo.com");
 
         Factura f1 = new Factura(1, 1, 1, "", "1000", "hoy", "18:30", 1000);
+        c1.getFacturas().add(f1);
+        System.out.println("Fecha factura " + c1.getFacturas().get((0)));
 
     }
 }
